f25d257695530c00251347a9c2f48291ac8fb48d
07_Abdulaziz Mac_SEI-14 <aziz@s1.sa>
2021-01-29 03:52:30 +0300

    Revert "Setting up Home page component (#9)" (#12)
    This reverts commit e0263b839fe9f2fc75b4a7c08880a9ee5834f8d1.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,9 @@
-import Home from './components/Home.js'
 import Developers from "./components/Developers";
 
 function App() {
   return (
     <div className="App">
-      <Home/>
-      {/* <Developers/> */}
+     {/* <Developers/> */}
     </div>
   );
 }
