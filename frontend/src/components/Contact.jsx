5cae58cf04597bcec42eaad3082c5521ecd22d2a
31_Fahad Mac_SEL-14 <alsaedifahad58@gmail.com>
2021-02-02 22:10:04 +0300

    Contact Frontend (#44)
    * this is for the axios on react for the contact page
    
    * update the vlues to database
    
    * Rename Labels and Message Input Name
    
    Remove state from Contact component
    
    Co-authored-by: 07_Abdulaziz Mac_SEI-14 <aziz@s1.sa>

--- frontend/src/components/Contact.jsx
+++ frontend/src/components/Contact.jsx
@@ -1,14 +1,24 @@
 import React, { Component } from "react";
 import { Form, Input, Button, Divider } from 'antd';
+import axios from 'axios'
 import '../App.css'
 const { TextArea } = Input;
+
 class Contact extends Component {
   constructor(props) {
     super(props)
-
-    this.state = {
-    }
   }
+  postContact = (values) => {
+    axios.post(`http://localhost:3000/contact`,
+       values)
+      .then((response) => {
+        console.log("RESPONSE: ", response);
+      })
+      .catch((err) => {
+        console.log("ERR: ", err);
+      });
+    };
+   
   render() {
     const layout = {
       labelCol: { span: 8 },
@@ -18,6 +28,7 @@ class Contact extends Component {
       wrapperCol: { offset: 8, span: 16 },
     };
     const onFinish = (values) => {
+      this.postContact(values);
       console.log('Success:', values);
     };
     const onFinishFailed = (errorInfo) => {
@@ -42,33 +53,41 @@ class Contact extends Component {
           >
             <Form.Item
               label="Firstname"
-              name="Firstname"
+              name="firstname"
               rules={[{ required: true, message: 'Please input your firstname!' }]}
+               
             >
               <Input />
             </Form.Item>
             <Form.Item
               label="Lastname"
-              name="Lastname"
+              name="lastname"
               rules={[{ required: true, message: 'Please input your Lastname!' }]}
+               
             >
               <Input />
             </Form.Item>
 
             <Form.Item
-              label="email"
+              label="E-mail"
               name="email"
               rules={[{ required: true, message: 'Please input your email!' }]}
+              
             >
               <Input />
             </Form.Item>
 
             <Form.Item {...tailLayout} >
-              <TextArea placeholder='Write something..' rows={4} />
+              <TextArea placeholder='Write something..' rows={4} 
+               label="Messagee"
+               name="message"
+              />
             </Form.Item>
 
             <Form.Item {...tailLayout}>
-              <Button type="primary" htmlType="submit">
+              <Button type="primary" htmlType="submit"
+              
+              >
                 Send
             </Button>
             </Form.Item>
