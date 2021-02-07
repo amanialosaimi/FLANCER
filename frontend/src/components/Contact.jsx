5cb313743757d481c9736030418a4c333191407f
s1 <aziz@s1.sa>
2021-02-07 03:44:38 +0300

    Prettier Contact Component

--- frontend/src/components/Contact.jsx
+++ frontend/src/components/Contact.jsx
@@ -16,75 +16,75 @@ const style = {
   fontSize: 14,
 };
 function Contact() {
-    const layout = {
-      labelCol: { span: 8 },
-      wrapperCol: { span: 16 },
-    };
-    const tailLayout = {
-      wrapperCol: { offset: 8, span: 16 },
-    };
-    const onFinish = (values) => {
-      API.postContact(values).then((result) => console.log("Message Saved: ", result))
-    };
-    const onFinishFailed = (errorInfo) => {
-      console.log('Failed:', errorInfo);
-    };
-    return (
-      <div className="contContainer site-card-wrapper">
-        <Layout style={{ padding: "0 24px 24px" }} className="site-layout">
-          <HeaderContent title={'CONTACT US'} h2={"Let's keep in touch!"} />
-          <div className='contact-container'>
-            <Form
-              {...layout}
-              name="basic"
+  const layout = {
+    labelCol: { span: 8 },
+    wrapperCol: { span: 16 },
+  };
+  const tailLayout = {
+    wrapperCol: { offset: 8, span: 16 },
+  };
+  const onFinish = (values) => {
+    API.postContact(values).then((result) => console.log("Message Saved: ", result))
+  };
+  const onFinishFailed = (errorInfo) => {
+    console.log('Failed:', errorInfo);
+  };
+  return (
+    <div className="contContainer site-card-wrapper">
+      <Layout style={{ padding: "0 24px 24px" }} className="site-layout">
+        <HeaderContent title={'CONTACT US'} h2={"Let's keep in touch!"} />
+        <div className='contact-container'>
+          <Form
+            {...layout}
+            name="basic"
 
-              onFinish={onFinish}
-              onFinishFailed={onFinishFailed}
+            onFinish={onFinish}
+            onFinishFailed={onFinishFailed}
+          >
+            <Form.Item
+              label={<h4><b>First Name</b></h4>}
+              name="firstname"
+              rules={[{ required: true, message: 'Please input your firstname!' }]}
+
             >
-              <Form.Item
-                label={<h4><b>First Name</b></h4>}
-                name="firstname"
-                rules={[{ required: true, message: 'Please input your firstname!' }]}
+              <Input />
+            </Form.Item>
+            <Form.Item
+              label={<h4><b>Last Name</b></h4>}
+              name="lastname"
+              rules={[{ required: true, message: 'Please input your Lastname!' }]}
 
-              >
-                <Input />
-              </Form.Item>
-              <Form.Item
-                label={<h4><b>Last Name</b></h4>}
-                name="lastname"
-                rules={[{ required: true, message: 'Please input your Lastname!' }]}
+            >
+              <Input />
+            </Form.Item>
 
-              >
-                <Input />
-              </Form.Item>
+            <Form.Item
+              label={<h4><b>Email</b></h4>}
+              name="email"
+              rules={[{ required: true, message: 'Please input your email!' }]}
 
-              <Form.Item
-                label={<h4><b>Email</b></h4>}
-                name="email"
-                rules={[{ required: true, message: 'Please input your email!' }]}
+            >
+              <Input />
+            </Form.Item>
 
-              >
-                <Input />
-              </Form.Item>
+            <Form.Item {...tailLayout} >
+              <TextArea placeholder='Write something..' rows={4}
+                label="Messagee"
+                name="message"
+              />
+            </Form.Item>
 
-              <Form.Item {...tailLayout} >
-                <TextArea placeholder='Write something..' rows={4}
-                  label="Messagee"
-                  name="message"
-                />
-              </Form.Item>
-
-              <Form.Item {...tailLayout}>
-                <Button type="primary" htmlType="submit" style={style}>
-                  Send
+            <Form.Item {...tailLayout}>
+              <Button type="primary" htmlType="submit" style={style}>
+                Send
             </Button>
-              </Form.Item>
-            </Form>
-          </div>
-        </Layout>
-      </div>
-    );
-  }
+            </Form.Item>
+          </Form>
+        </div>
+      </Layout>
+    </div>
+  );
+}
 export default Contact;
 
 
